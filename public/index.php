452504a2f0622e61c88885c2445e5349420ab255
Jacob Ransom <snakeransom@gmail.com>
2021-04-14 11:51:06 -0700

    Fixing indentation issue.

--- public/index.php
+++ public/index.php
@@ -1,11 +1,11 @@
 <?php
-    require __DIR__ . '/../vendor/autoload.php';
-    $f3 = \Base::instance();
-    $f3->set('AUTOLOAD', __DIR__ . '/../src/');
-    $f3->route('GET /', 'Controllers\DateCalculator->getMainPage');
+	require __DIR__ . '/../vendor/autoload.php';
+	$f3 = \Base::instance();
+	$f3->set('AUTOLOAD', __DIR__ . '/../src/');
+	$f3->route('GET /', 'Controllers\DateCalculator->getMainPage');
 	$f3->route('POST /calculate-from-date', 'Controllers\DateCalculator->calculateFromDate');
 	$f3->route('POST /calculate-from-time', 'Controllers\DateCalculator->calculateFromTime');
 	$f3->route('POST /calculate-diff-date', 'Controllers\DateCalculator->calculateDiffDate');
 	$f3->route('POST /calculate-diff-time', 'Controllers\DateCalculator->calculateDiffTime');
-    $f3->run();
+	$f3->run();
 ?>
